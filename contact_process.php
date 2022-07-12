42936a6365312e077b10772383900b8e26c15cf5
Richie <65792694+arunike@users.noreply.github.com>
2022-07-12 12:40:52 -0400

    Add files via upload

--- contact_process.php
+++ contact_process.php
@@ -1,10 +1,8 @@
 <?php
-
-    $to = "rockybd1995@gmail.com";
+    $to = "richiezhouyjz@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
     $cmessage = $_REQUEST['message'];
 
     $headers = "From: $from";
@@ -13,7 +11,7 @@
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your Bitmap Photography.";
+    $subject = "You have a message from your Richie Zhou";
 
     $logo = 'img/logo.png';
     $link = '#';
@@ -33,5 +31,4 @@
 	$body .= "</body></html>";
 
     $send = mail($to, $subject, $body, $headers);
-
 ?>

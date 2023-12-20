9c41b3b40c8d701167b7182fa3942b2193e6fab3
Richie Zhou <r631137986@gmail.com>
2023-12-20 17:07:28 -0600

    updated configs

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: { 
+    chunkSizeWarningLimit: 1600, 
+    outDir: 'build',
+  }
 })

2d3435aa80835eeb09c9c2acb980750bfa78fbdb
Richie Zhou <r631137986@gmail.com>
2024-05-19 01:42:41 -0400

    remove typo

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -215,7 +215,7 @@ function Home() {
 
               <Col md={4} className="myAvatar">
                 <Tilt>
-                  <img src={myImg} alt="avatar" className="img-fluid" alt="avatar"/>
+                  <img src={myImg} alt="avatar" className="img-fluid"/>
                 </Tilt>
               </Col>
             </Row>
